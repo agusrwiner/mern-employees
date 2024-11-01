276512fee4f200c63965affe469a8afd39d74741
Agustin R. Winer <r.w.agustin@gmail.com>
2024-11-01 12:10:10 -0300

    RecordLis.jsx endpoint update

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -41,7 +41,7 @@ export default function RecordList() {
     // Fetches the records from the database
     useEffect(() => {
         async function getRecords() {
-            const response = await fetch(`http://localhost:5050/record/`);
+            const response = await fetch(`https://mern-employees.vercel.app/record/`);
             if (!response.ok) {
                 const message = `Error: ${response.statusText}`;
                 console.error(message);
@@ -56,7 +56,7 @@ export default function RecordList() {
 
     // Delete a record
     async function deleteRecord(id) {
-        await fetch(`http://localhost:5050/record/${id}`, {
+        await fetch(`https://mern-employees.vercel.app/record/${id}`, {
             method: "DELETE",
         });
         const newRecords = records.filter((el) => el._id !== id);
